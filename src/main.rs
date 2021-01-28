438280a9e328c2fe8dece52beaf1c606a37df1f0
Rafael Knoll <Rafihknoll@gmail.com>
2021-01-28 17:02:11 +0200

    Added set_next method

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,18 @@ impl<T> LinkedList<T> {
 	pub fn new(item: T) -> LinkedList<T> {
 		LinkedList::Node(item, Box::new(LinkedList::End))
 	}
+
+	fn set_next(&mut self, new_target: LinkedList<T>) -> () {
+		match self {
+			LinkedList::Node(_, ref mut next) => {*next = Box::new(new_target)},
+			LinkedList::End => panic!("Attempt to concat to the end of a list!"),
+		};
+	}
 }
 
 fn main() {
-    let x = LinkedList::new(5);
-	println!("{:?}", x)
+    let mut x = LinkedList::new(5);
+	let y = LinkedList::new(7);
+	x.set_next(y);
+	println!("{:?}", x);
 }
